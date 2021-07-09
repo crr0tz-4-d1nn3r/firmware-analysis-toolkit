58be1c7395e2c310d641de033f0c6b2076f69d59
crr0tz-4-d1nn3r <crr0tz@example.com>
2021-07-09 09:28:47 -0400

    build for Kali 2021'

--- setup.sh
+++ setup.sh
@@ -2,15 +2,17 @@
 
 set -e
 sudo apt update
-sudo apt install -y python-pip python3-pip python3-pexpect unzip busybox-static fakeroot kpartx snmp uml-utilities util-linux vlan qemu-system-arm qemu-system-mips qemu-system-x86 qemu-utils
+sudo apt install -y python3-pip python3-pexpect unzip busybox-static fakeroot kpartx snmp uml-utilities util-linux vlan qemu-system-arm qemu-system-mips qemu-system-x86 qemu-utils
 
 echo "Installing binwalk"
 git clone --depth=1 https://github.com/devttys0/binwalk.git
 cd binwalk
+sed -i -e 's/qt5base/qtbase5/g' deps.sh
+sed -i -e 's/cd sasquatch && \$SUDO .\/build.sh/cd sasquatch \&\& CFLAGS=-fcommon \$SUDO .\/build.sh/g' deps.sh
 sudo ./deps.sh --yes
 sudo python3 ./setup.py install
 sudo -H pip3 install git+https://github.com/ahupp/python-magic
-sudo -H pip install git+https://github.com/sviehb/jefferson
+sudo -H pip3 install git+https://github.com/sviehb/jefferson
 cd ..
 
 echo "Installing firmadyne"
